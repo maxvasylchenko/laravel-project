a227a9c5c139c3ceede3e1470ad9dab7cf83bb0b
maxvasylchenko <maksymvasylchenko@mim.website>
2023-01-28 17:54:01 +0200

    intro

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'surname' => fake()->lastName(),
+            'birthday' => fake()->dateTimeBetween('-70 years', '-18 years')->format('Y-m-d'),
+            'phone' => fake()->unique()->e164PhoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make ('max123'), // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (User $user) {
+                $user->assignRole('customer');
+        });
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return static
+     */
+    public function unverified()
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+
+    public function withEmail(string $email)
+    {
+        return $this->state(function (array $attrs) use ($email) {
+           return [
+               'email' => $email
+           ];
+        });
+    }
+}
