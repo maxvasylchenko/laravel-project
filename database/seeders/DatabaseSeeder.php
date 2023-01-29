7246c7c91cbe2712c934e9808eb666892958e5bf
maxvasylchenko <113374488+maxvasylchenko@users.noreply.github.com>
2023-01-29 10:45:19 +0200

    Revert "laravel_2 (#1)" (#2)
    This reverts commit c49377a93d62bc92054e27056e45730b2c32e68d.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(PermissionsSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
