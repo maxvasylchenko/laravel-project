15d6744f68f8b09a26e29717df4a74d2973d4ceb
maxvasylchenko <maksymvasylchenko@mim.website>
2023-02-01 18:02:55 +0200

    hw21

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const USER_ACCOUNT = '/home';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,8 +30,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
-                    </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('products.index') }}">{{ __('Products') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('categories.index') }}">{{ __('Categories') }}</a>
+                        </li>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
